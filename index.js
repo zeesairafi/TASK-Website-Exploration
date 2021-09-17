a6068816cb41f566511135b3f26301cc8ad35d85
Octowl <alsaff1987@gmail.com>
2021-09-18 00:55:36 +0300

    simplify js

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-let appearance = "light";
+let isDark = false;
 
-// 1. This function will toggle the appearance from white text to dark text
+// 1. This function will toggle the appearance from light text to dark text
 function toggleColor() {
-  if (appearance == "light") {
-    appearance = "dark";
-    const body = document.getElementsByTagName("body")[0];
+  const body = document.getElementsByTagName("body")[0];
+  isDark = !isDark;
+  if (isDark) {
     body.style.color = "black";
   } else {
-    appearance = "light";
-    const body = document.getElementsByTagName("body")[0];
     body.style.color = "white";
   }
 }
 
-// 2. This will make everything lower-cased
+// 2. This will make everything lowercase
 function beGentle() {
   document.getElementsByTagName("body")[0].style.textTransform = "lowercase";
 }
 
-// 3. This will make everything upper-cased.
+// 3. This will make everything uppercase.
 // if you read this line, then you solved the task!
 // add shout() inside the quotation marks after onclick in index.html like this.
 // <button onclick="shout()">SHOUT!</button>
@@ -27,16 +25,15 @@ function shout() {
 }
 
 // 4. This will fetch a random name from the internet, and replace your name with it!
-function fetchRandomName() {
-  fetch("https://randomuser.me/api/", {
-    method: "GET",
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const name = data.results[0].name.first;
-      document.getElementsByTagName("h3")[0].innerText = name;
-    })
-    .catch((err) => {
-      alert(`Couldn't make the request, ${err.message}`);
+async function fetchRandomName() {
+  try {
+    const response = await fetch("https://randomuser.me/api/", {
+      method: "GET",
     });
+    const data = await response.json();
+    const name = data.results[0].name.first;
+    document.getElementsByTagName("h3")[0].innerText = name;
+  } catch (err) {
+    alert(`Couldn't make the request, ${err.message}`);
+  }
 }
