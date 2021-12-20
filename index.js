e40edac3237e559927f95845cd1928ae17f90b1a
kawthar alsairafi <kawtheralsairafi@kawthars-MacBook-Air.local>
2021-12-20 15:58:05 +0300

    finished my task

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function toggleColor() {
   const body = document.getElementsByTagName("body")[0];
   isDark = !isDark;
   if (isDark) {
-    body.style.color = "black";
+    body.style.color = "grey";
   } else {
-    body.style.color = "white";
+    body.style.color = "pink";
   }
 }
 
